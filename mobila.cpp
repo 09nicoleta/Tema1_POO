d47cdcfcb0730e179e2b4b053ba67e5f607044b5
09nicoleta <nicoleta.musat1@s.unibuc.ro>
2026-03-18 15:04:29 +0200

    Am creat setters pentru clasele Clienti, Componente si am supraincarcat operatorii de citire pentru clasele Mobila si Clienti - citirea datelor se va face din fisier.

--- mobila.cpp
+++ mobila.cpp
@@ -1,14 +1,35 @@
 #include<iostream>
 #include<utility>
 #include<string>
+#include<fstream>
 
 class Clienti{
     
     std::string mobila;
-    unsigned int nr_cl;
+    int nr_cl;
+    
     public:
-    Clienti(const std::string& mob, unsigned int nr_cl=0):
+    Clienti(const std::string& mob="", unsigned int nr_cl=0):
     mobila(mob), nr_cl{nr_cl} {};
+
+    void setMobila(const std::string& mobila){
+        this->mobila=mobila;
+    }
+
+    void setNrClienti(int nr_cl){
+        this->nr_cl=nr_cl;
+    }
+
+    friend std::istream& operator >>(std::istream & in, Clienti client){
+        std::string tip;
+        int nrc;
+        std::getline(in,tip);
+        client.mobila=tip;
+        in>>nrc;
+        client.nr_cl=nrc;
+        return in;
+    }
+
 };
 
 class Componente{
@@ -22,22 +43,74 @@ class Componente{
     Componente(Componente &&comp) : 
     nr_piese{std::move(comp.nr_piese)}, comp_princip{std::move(comp.comp_princip)} {};
     
+    void setNr_piese(unsigned int nr){
+        nr_piese=nr;
+    }
+
+    void setComp(const std::string& comp){
+        comp_princip=comp;
+    }
+    
+
 };
 
 class Mobila{
-    Componente comp;
-    unsigned int stoc,pret; 
     std::string tip;
+    Componente comp;
+    unsigned int stoc, pret; 
     
     public:
+    Mobila(const std::string& tip_="", Componente &&comp_={0,""}, unsigned int stoc_=0, unsigned int pret_=0):
+    tip{tip_}, comp{std::move(comp_)}, stoc{stoc_}, pret{pret_} {};
 
-    Mobila(unsigned int stoc_=0, unsigned int pret_=0, const std::string& tip_="", Componente &&comp_={0,""}):
-    stoc{stoc_}, pret{pret_}, tip{tip_}, comp{std::move(comp_)}{};
+    friend std::istream& operator>>(std::istream &in, Mobila& mob ){ 
+        std::string linie;
+        int x;
+        std::getline(in,linie);
+        mob.tip=linie;
 
+        in>>x;
+        std::getline(in,linie);
+        mob.comp.setComp(linie);
+        mob.comp.setNr_piese(x);
+
+        in>>x;
+        mob.stoc=x; 
+        in>>x;
+        mob.pret=x;       
+        return in;
+    }
+
+   friend std::ostream& operator<<(std::ostream & out, const Mobila& mob){ 
+
+    
+    return out;
+    }
+ 
 };
+
+
 
 int main(){
 
+    int total_mob,i;
+    
+    Mobila mobila[100];
+    Clienti cli[100];
+    
+    std::ifstream f("date.in");
+    if(!f){
+        std::cout<<"Eroare - nu se poate deschide fisierul";
+    }
+    else{
+    f>>total_mob;
 
+    for(i=0;i<total_mob;++i){
+
+    }
+
+    f.close();
+
+}
     return 0;
 }
